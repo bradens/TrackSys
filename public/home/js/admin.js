b40053b021b89acf54dd064c9f66f4a7c8a8f082
Braden Simpson <braden@uvic.ca>
2011-11-18 23:17:31 -0800

    Added jquery ui

---
+++ public/home/js/admin.js
@@ -0,0 +1,13 @@
+var admin = {
+		init: function()
+		{
+			/*$("#accordion").accordion();
+			
+			$('.accordion .head').click(function() {
+				$(this).next().toggle('slow');
+				return false;
+			}).next().hide();*/
+			$("#tabs").tabs();
+		}
+		
+}
