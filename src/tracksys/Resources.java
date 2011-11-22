d4254541fe4ad2b32f386d205a0cf55cbf62ea58
Braden Simpson <braden@uvic.ca>
2011-11-21 21:34:49 -0800

    Added new dump

--- src/tracksys/Resources.java
+++ src/tracksys/Resources.java
@@ -14,9 +14,9 @@ public class Resources {
 	public static String COOKIE_CLUBID = "clubID";
 	public static int ADMIN_ID = 0;
 	public static int LOGGED_OUT = -1;
-	public static String username = "modus";
-	public static String password = "pwnens";
-	public static String database = "jdbc:mysql://bradensimpson.com";
+	public static String username = "root";
+	public static String password = "root";
+	public static String database = "jdbc:mysql://localhost";
 	public static String NOTIFICATION_TITLE_PARAM = "title";
 	public static String NOTIFICATION_MESSAGE_PARAM = "message";
 	public static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
