963a5dddf108b54254e22c2acce0a82cec67db08
Jordan Ell <jordan.ell7@gmail.com>
2011-11-22 15:23:37 -0800

    Validation works for recurring bookings.

--- src/tracksys/boundary/views/HomeView.java
+++ src/tracksys/boundary/views/HomeView.java
@@ -244,6 +244,8 @@ public class HomeView {
 			int track = generator.nextInt(8) + 1;
 			int clubid = manager.getClubIDFromCookie(req);
 			
+			boolean recure = true;
+			
 			if(recurring.equalsIgnoreCase("true"))
 			{
 				int i = 4;
@@ -251,7 +253,10 @@ public class HomeView {
 				while(i > 0)
 				{
 					Booking booking = new Booking(clubid, "", track, startDate, endDate, stamp, comment);
-					manager.addBooking(booking);
+					if(manager.addBooking(booking))
+						recure = recure;
+					else
+						recure = false;
 					
 					// Add to start date
 					c.setTime(startDate);
@@ -265,6 +270,11 @@ public class HomeView {
 					
 					i--;
 				}
+				
+				if(recure)
+					ServletHandler.writeResponse("true", resp);
+				else
+					ServletHandler.writeResponse("false", resp);
 			}
 			else
 			{
