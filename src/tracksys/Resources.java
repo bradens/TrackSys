e2c22a9dcb8e7d274ca401e8fd6d55adddb2771b
Braden Simpson <braden@uvic.ca>
2011-11-28 20:41:02 -0800

    started club name filter

--- src/tracksys/Resources.java
+++ src/tracksys/Resources.java
@@ -14,14 +14,14 @@ public class Resources {
 	public static int ADMIN_ID = 0;
 	public static int LOGGED_OUT = -1;
 	
-//	public static String username = "modus";
-//	public static String password = "pwnens";
-//	public static String database = "jdbc:mysql://bradensimpson.com";
+	public static String username = "modus";
+	public static String password = "pwnens";
+	public static String database = "jdbc:mysql://bradensimpson.com";
 	
 	// Comment these out if you want to use online db
-	public static String username = "root";
-	public static String password = "root";
-	public static String database = "jdbc:mysql://localhost";
+//	public static String username = "root";
+//	public static String password = "root";
+//	public static String database = "jdbc:mysql://localhost";
 	
 	public static String NOTIFICATION_TITLE_PARAM = "title";
 	public static String NOTIFICATION_MESSAGE_PARAM = "message";
